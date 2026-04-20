49d9bdae367a140b9b2ef9e38f9232dac04162a9
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-20 15:38:24 +0500

    🚀 Add main application code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function setMatrixZeroes(matrix) {
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    let rowZero = new Array(rows).fill(false);
+    let colZero = new Array(cols).fill(false);
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (matrix[i][j] === 0) {
+                rowZero[i] = true;
+                colZero[j] = true;
+            }
+        }
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (rowZero[i] || colZero[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    return matrix;
+}
